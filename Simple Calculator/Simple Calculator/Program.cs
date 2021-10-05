aaf5c91903aada65385a121905e74aa78a982c6c
Yerkanat Sheriyazdanov <56153705+legion2809@users.noreply.github.com>
2021-10-06 01:43:35 +0600

    Updating other  project files

--- Simple Calculator/Simple Calculator/Program.cs
+++ Simple Calculator/Simple Calculator/Program.cs
@@ -16,7 +16,7 @@ namespace Simple_Calculator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
